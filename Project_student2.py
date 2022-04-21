0c44714f940ad903ee66edd008055e584c7dacd7
dsfaqfq <hugo.vandermosten1@gmail.com>
2022-04-21 23:39:36 +0200

    final

--- Project_student2.py
+++ Project_student2.py
@@ -18,21 +18,10 @@ LORENZ SYSTEM
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-import matplotlib.colors as colors
-from mpl_toolkits.mplot3d import Axes3D
-from numpy import pi
-import random
 
-
-def L1(X, Y):
-    distance = abs(X - Y)
-    return np.sum(distance)
-
-
-def Linf(X, Y):
-    distance = abs(X - Y)
-    return np.max(distance)
-
+"""
+Fonction plutot utile partout
+"""
 
 def makeLorentz(params):
     def Lorenz(state, t):
@@ -41,45 +30,30 @@ def makeLorentz(params):
 
     return Lorenz
 
+"""
 
-n = 100
-distancesL1 = np.zeros(n)
-distancesLinf = np.zeros(n)
-params = np.zeros((100, 3))
-params[:, 0] = np.linspace(10, 5, n)
-params[:, 1] = np.linspace(28, 28, n)
-params[:, 2] = np.linspace(8 / 3, 8 / 3, n)
+Partie 1.1.1
 
+"""
+
+params = [10.0, 28.0, 8.0 / 3.0]
 state0 = [1.0, 1.0, 1.0]  # initial condition
 t = np.arange(0.0, 100.0, 0.02)  # time vector
-states0 = odeint(makeLorentz(params[0]), state0, t)  # vector containing the (x,y,z) positions for each time step
-squares = np.int_(np.floor(states0 / 5))
-matrix0 = np.zeros((8, 12, 10))
+states = odeint(makeLorentz(params), state0, t)  # vector containing the (x,y,z) positions for each time step
+squares = np.int_(np.floor(states / 5))
+matrix = np.zeros((8, 12, 10))
 for elem in squares:
     square = tuple(elem)
     x, y, z = square
-    matrix0[x + 4][y + 6][z] += 1
+    matrix[x + 4][y + 6][z] += 1
 
-for i in range(n):
-    states = odeint(makeLorentz(params[i]), state0, t)  # vector containing the (x,y,z) positions for each time step
-    squares = np.int_(np.floor(states / 5))
-    matrix = np.zeros((8, 12, 10))
-    for elem in squares:
-        square = tuple(elem)
-        x, y, z = square
-        matrix[x + 4][y + 6][z] += 1
-    distancesL1[i] = L1(matrix0, matrix)
-    distancesLinf[i] = Linf(matrix0, matrix)
+xy = np.sum(matrix, axis=2)
+yz = np.sum(matrix, axis=0)
+xz = np.sum(matrix, axis=1)
 
-print(distancesL1)
-plt.plot(np.linspace(10, 5, 100), distancesL1, label="L1")
-plt.plot(np.linspace(10, 5, 100), distancesLinf, label="Linf")
-plt.xlabel("sigma")
-plt.legend()
-plt.grid()
+x, y, z = np.linspace(-20, 20, 9), np.linspace(-30, 30, 13), np.linspace(0, 50, 11)
 
-plt.show()
-"""
+
 plt.title("Projection on the xy space")
 plt.pcolormesh(x, y, xy.T / 5000)
 plt.xlabel("x")
@@ -103,40 +77,110 @@ plt.ylabel("z")
 plt.colorbar()
 plt.savefig("pdf_yz.png")
 plt.show()
-"""
+
+
 
 """
-fig = plt.figure()
-ax = fig.gca(projection="3d")
-ax.plot(states[:, 0], states[:, 1], states[:, 2])
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend(['True system'])
-plt.draw()
+
+Partie 1.1.2
+
+"""
+
+def L1(X, Y):
+    distance = abs(X - Y)
+    return np.sum(distance)
+
+
+def Linf(X, Y):
+    distance = abs(X - Y)
+    return np.max(distance)
+
+
+"""
+
+partie 1.1.3
+
+"""
+
+n = 100
+distancesL1 = np.zeros(n)
+distancesLinf = np.zeros(n)
+
+params = np.zeros((n, 3))
+params[:, 0] = np.linspace(10, 5, n)
+params[:, 1] = np.linspace(28, 28, n)
+params[:, 2] = np.linspace(8/3, 8/3, n)
+state0 = [1, 1, 1]
+
+t = np.arange(0.0, 100.0, 0.02)  # time vector
+states = odeint(makeLorentz(params[0]), state0, t)  # vector containing the (x,y,z) positions for each time step
+squares = np.int_(np.floor(states / 5))
+matrix0 = np.zeros((8, 12, 10))
+for elem in squares:
+    square = tuple(elem)
+    x, y, z = square
+    matrix0[x + 4][y + 6][z] += 1
+
+for i in range(n):
+    states = odeint(makeLorentz(params[i]), state0, t)  # vector containing the (x,y,z) positions for each time step
+    squares = np.int_(np.floor(states / 5))
+    matrix = np.zeros((8, 12, 10))
+    for elem in squares:
+        square = tuple(elem)
+        x, y, z = square
+        matrix[x + 4][y + 6][z] += 1
+    distancesL1[i] = L1(matrix0, matrix)
+    distancesLinf[i] = Linf(matrix0, matrix)
+
+plt.plot(np.linspace(10, 5, n), distancesL1, label="L1")
+plt.plot(np.linspace(10, 5, n), distancesLinf, label="Linf")
+plt.xlabel("sigma")
+plt.ylabel("distance")
+plt.title("distance between the two statistical distributions")
+plt.legend()
+plt.grid()
 plt.show()
-"""
 
 """
-PLOTLY : TRUE SYSTEM
-"""
 
-# Uncomment this section once you've installed the "Plotly" package
+partie 1.1.4
 
 """
-import plotly.graph_objects as go
-import plotly.io as pio
-pio.renderers.default = "browser"
+n = 100
+distancesL1 = np.zeros(n)
+distancesLinf = np.zeros(n)
 
+params = [10.0, 28.0, 8.0 / 3.0]
+states0 = np.zeros((n, 3))
+states0[:, 0] = np.linspace(1, 10, n)
+states0[:, 1] = np.linspace(1, 10, n)
+states0[:, 2] = np.linspace(1, 10, n)
 
-fig = go.Figure(data=[go.Scatter3d(x=states[:, 0],y=states[:, 1],z=states[:, 2],
-                                   mode='markers',
-                                   marker=dict(
-                                       size=2,
-                                       opacity=0.8
-    )                        
-                                   )])
-fig.update_layout(
-    title='True system')
-fig.update_scenes(aspectmode='data')
-fig.show()
-"""
+t = np.arange(0.0, 100.0, 0.02)  # time vector
+states = odeint(makeLorentz(params), states0[0], t)  # vector containing the (x,y,z) positions for each time step
+squares = np.int_(np.floor(states / 5))
+matrix0 = np.zeros((8, 12, 10))
+for elem in squares:
+    square = tuple(elem)
+    x, y, z = square
+    matrix0[x + 4][y + 6][z] += 1
+
+for i in range(n):
+    statesi = odeint(makeLorentz(params), states0[i], t)  # vector containing the (x,y,z) positions for each time step
+    squares = np.int_(np.floor(statesi / 5))
+    matrix = np.zeros((8, 12, 10))
+    for elem in squares:
+        square = tuple(elem)
+        x, y, z = square
+        matrix[x + 4][y + 6][z] += 1
+    distancesL1[i] = L1(matrix0, matrix)
+    distancesLinf[i] = Linf(matrix0, matrix)
+
+plt.plot(np.linspace(1, 10, n), distancesL1, label="L1")
+plt.plot(np.linspace(1, 10, n), distancesLinf, label="Linf")
+plt.xlabel("initial state coordinates")
+plt.ylabel("distance")
+plt.title("distance between the two statistical distributions")
+plt.legend()
+plt.grid()
+plt.show()
